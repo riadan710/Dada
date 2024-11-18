a7a01aafd5b90e67759770e56c8ecbbaf43b0d92
riadan710 <riadan710@gmail.com>
2024-11-18 15:34:09 +0900

    [Feat] BottomNavigationView 구현

--- app/src/main/java/com/experts/dada/MainActivity.kt
+++ app/src/main/java/com/experts/dada/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.experts.dada
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.experts.dada.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 바텀 네비게이션 뷰 작동
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, DiaryFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_diary -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, DiaryFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.fragment_star -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, StarFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.fragment_store -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, StoreFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.fragment_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SettingFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                else -> false
+            }
         }
     }
 }
