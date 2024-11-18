bc9596dec8193b44c87ef21eed129d839f267c48
riadan710 <riadan710@gmail.com>
2024-11-19 00:24:24 +0900

    [Feat] DiaryFragment 생성 및 캘린더 선택한 날짜 데이터 전달

--- app/src/main/java/com/experts/dada/DiaryFragment.kt
+++ app/src/main/java/com/experts/dada/DiaryFragment.kt
@@ -1,18 +1,42 @@
 package com.experts.dada
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
+import com.experts.dada.databinding.FragmentDiaryBinding
 
 class DiaryFragment : Fragment() {
+
+    lateinit var binding: FragmentDiaryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+        binding = FragmentDiaryBinding.inflate(inflater, container, false)
+
+        binding.diaryCv.setOnDateChangeListener(object : CalendarView.OnDateChangeListener {
+            override fun onSelectedDayChange(
+                view: CalendarView,
+                year: Int,
+                month: Int,
+                dayOfMonth: Int
+            ) {
+                Toast.makeText(activity, "$year. ${month + 1}. $dayOfMonth", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(activity, MemoActivity::class.java)
+                intent.putExtra("year", year)
+                intent.putExtra("month", month)
+                intent.putExtra("dayOfMonth", dayOfMonth)
+                startActivity(intent)
+            }
+        })
+
+        return binding.root
     }
 }
