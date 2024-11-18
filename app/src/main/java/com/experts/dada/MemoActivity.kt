49a9e2060fc90b72567bebb3ec004274aee88519
riadan710 <riadan710@gmail.com>
2024-11-18 18:10:42 +0900

    [Feat] MemoActivity 추가

---
+++ app/src/main/java/com/experts/dada/MemoActivity.kt
@@ -0,0 +1,11 @@
+package com.experts.dada
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo)
+    }
+}
