ee685da0b620f61d88a6d581120152079874942a
riadan710 <riadan710@gmail.com>
2024-11-19 21:30:13 +0900

    [Feat] 설정 화면 레이아웃 수정

--- app/src/main/java/com/experts/dada/SettingFragment.kt
+++ app/src/main/java/com/experts/dada/SettingFragment.kt
@@ -1,17 +1,29 @@
 package com.experts.dada
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.experts.dada.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
+
+    lateinit var binding: FragmentSettingBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+
+        binding.settingLogoutLl.setOnClickListener {
+            val intent = Intent(requireContext(), StartActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        return binding.root
     }
 }
