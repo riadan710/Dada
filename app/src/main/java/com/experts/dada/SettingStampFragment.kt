9660647a916163d8014301533dbc945c23b8506c
riadan710 <riadan710@gmail.com>
2024-11-29 17:06:14 +0900

    [Feat] 내 스탬프 페이지 스탬프 그리드뷰 추가

--- app/src/main/java/com/experts/dada/SettingStampFragment.kt
+++ app/src/main/java/com/experts/dada/SettingStampFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.experts.dada.databinding.FragmentSettingProfileBinding
 import com.experts.dada.databinding.FragmentSettingStampBinding
 
 class SettingStampFragment : Fragment() {
     lateinit var binding: FragmentSettingStampBinding
+    private lateinit var adapter: StoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSettingStampBinding.inflate(inflater, container, false)
+
+        adapter = StoreAdapter(requireContext())
+        binding.stampContentGv.adapter = adapter
 
         binding.stampCloseIv.setOnClickListener {
             parentFragmentManager.popBackStack()
