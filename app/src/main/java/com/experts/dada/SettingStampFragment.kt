9ddb01b945e94884ebec8ad0e36f842ec50bb1e8
riadan710 <riadan710@gmail.com>
2024-12-05 23:08:27 +0900

    [Feat] 설정 내 스탬프에 구매한 스탬프 표시

--- app/src/main/java/com/experts/dada/SettingStampFragment.kt
+++ app/src/main/java/com/experts/dada/SettingStampFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.experts.dada.databinding.FragmentSettingStampBinding
+import kotlinx.coroutines.launch
 
 class SettingStampFragment : Fragment() {
     lateinit var binding: FragmentSettingStampBinding
     private lateinit var adapter: StoreAdapter
+    private lateinit var stampDao: StampDao
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,10 +23,24 @@ class SettingStampFragment : Fragment() {
         adapter = StoreAdapter(requireContext())
         binding.stampContentGv.adapter = adapter
 
+        // StampDao 초기화
+        val diaryDatabase = DiaryDatabase.getDatabase(requireContext())
+        stampDao = diaryDatabase.stampDao()
+
+        // 구매한 스탬프 목록 불러오기
+        loadPurchasedStamps()
+
         binding.stampCloseIv.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
 
         return binding.root
+    }
+
+    private fun loadPurchasedStamps() {
+        lifecycleScope.launch {
+            val purchasedStamps = stampDao.getAllPurchasedStamps()
+            adapter.setStamps(purchasedStamps)
+        }
     }
 }
